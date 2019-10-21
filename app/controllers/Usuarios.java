5ee8b60e8dbe13cea1a166ac8a455b992ad1d7e6
Cimara <cimarinhaoliveira@hotmail.com>
2019-10-21 08:21:01 -0300

    tela inicial

---
+++ app/controllers/Usuarios.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import play.data.validation.Required;
+import play.mvc.Controller;
+
+@Entity
+public class Usuarios extends Controller{
+	
+	
+}
